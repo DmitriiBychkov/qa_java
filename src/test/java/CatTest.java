4f0b26622efe1c2e58547431d54f7f87d95fe261
DmitriiBychkov <bychkovdvjava@yandex.ru>
2022-10-30 14:24:17 +0300

    First commit

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,4 +1,33 @@
-package com.example;
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
+    @Spy
+    Feline feline;
+
+    //    Проверка голоса кота
+    @Test
+    public void getSoundReturnsMeow() {
+        Cat cat = new Cat(feline);
+        String expectedSound = "Мяу";
+        String actualSound = cat.getSound();
+        Assert.assertEquals("Кот говорит мяу", expectedSound, actualSound);
+    }
+
+    //    Проверка рациона кота
+    @Test
+    public void catGetFoodLikePredator() throws Exception {
+        Cat cat = new Cat(feline);
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        List<String> actualFood = cat.getFood();
+        Assert.assertEquals("Кот ест как Хищник", expectedFood, actualFood);
+    }
 }
